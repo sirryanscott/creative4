a6886044b8c5939da3f73cce80dcad265e918670
Ryan Struthers <ryan.s.struthers@gmail.com>
2018-03-12 06:55:51 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+		<router-view/>
+		<div id="footer">
+			<div id="github">
+				<a href="https://github.com/sirryanscott/creative4.git" target="_blank">
+					<img src="/static/images/GitHub_Logo.png"></img>
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#github {
+	width: 100px;
+}
+
+img {
+	margin-top: 200px;
+	width: 100%;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+	  <form>
+		  <h1>Login</h1>
+		  <label for="username">Username</label><br>
+		  <input type="text" placeholder="username"><br>
+		  <label for="password">Password</label><br>
+		  <input type="password" placeholder="password"><br>
+		  <router-link to="/"><button>Login</button></router-link>
+	  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+	text-align: center;
+	font-weight: normal;
+}
+
+form {
+	width: 400px;
+	margin: auto;
+	margin-top: 150px;
+	font-size: 20pt;
+}
+
+button, input {
+	box-sizing: border-box;
+	width: 100%;
+	padding: 8px;
+	font-size: 15pt;
+}
+
+input {
+	margin-top: 5px;
+	margin-bottom: 15px;
+	border-radius: 7px;
+}
+
+button {
+	border-radius: 20px;
+	color: #fff;
+	background: #D9002E;
+	border: solid;
+}
+	
+
+</style>
